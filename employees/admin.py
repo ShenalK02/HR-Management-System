cf9834ba21422d23a6a27eab045ca4ac96fa1336
ShenalK02 <shenalsdk@gmail.com>
2025-04-15 11:57:03 +0530

    Employee creation through Django admin configuration

--- employees/admin.py
+++ employees/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Employee, Department, Position
+from .forms import EmployeeCreationForm
 
-# Register your models here.
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    form = EmployeeCreationForm
+    list_display = ('username', 'email', 'position', 'is_hr')
+    list_filter = ('is_hr', 'position')
+
+
+admin.site.register(Department)
+admin.site.register(Position)
+
