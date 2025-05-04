ffec273d31f49f424e490dae7ba6684f6471f65b
Teshadi_Sen <teshsenanayake@gmail.com>
2025-05-04 14:37:54 +0530

    Update admin config for birthday model

--- birthday_calendar/admin.py
+++ birthday_calendar/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import BirthdayWish
 
-# Register your models here.
+@admin.register(BirthdayWish)
+class BirthdayWishAdmin(admin.ModelAdmin):
+    list_display = ['employee', 'celebration_date', 'celebration_style', 'created_by']
+    list_filter = ['celebration_date', 'celebration_style']
+    search_fields = ['employee__username', 'message', 'description']
+    date_hierarchy = 'celebration_date'
+    readonly_fields = ['created_at', 'updated_at']
+    
+    fieldsets = (
+        ('User Information', {
+            'fields': ('employee', 'created_by')
+        }),
+        ('Birthday Details', {
+            'fields': ('celebration_date', 'celebration_style', 'message', 'description')
+        }),
+        ('Metadata', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
