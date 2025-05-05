16a4882e37be81656cb668ea9359b9d0f7f5fe95
Oneli <oneli.ihansa@gmail.com>
2025-05-05 21:47:09 +0530

    Our team url added

--- employees/urls.py
+++ employees/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:pk>/', views.employee_detail, name='employee_detail'),
     path('create/', views.employee_create, name='employee_create'),
     path('<int:pk>/update/', views.employee_update, name='employee_update'),
+    path('our-team/', views.our_team, name='our_team'),
     
     # Department views
     path('departments/', views.department_list, name='department_list'),
